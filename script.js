2a39119716a939449a2adae4c3527fbadcfa9180
Kroatuan <88584837+Kenzohfs@users.noreply.github.com>
2021-12-01 19:28:14 -0300

    fetch dos pokemon

---
+++ script.js
@@ -0,0 +1,103 @@
+//https://prof-poke-api.herokuapp.com/api/pokedex/
+
+//https://prof-poke-api.herokuapp.com/api/pokemon/001/
+
+function criarLogo() {
+    const header = document.createElement('header');
+    const pokemonLogo = document.createElement('img');
+
+    header.style.display = 'flex';
+    header.style.justifyContent = 'center';
+
+    pokemonLogo.src = 'https://logosmarcas.net/wp-content/uploads/2020/05/Pokemon-Logo.png';
+    pokemonLogo.style.width = '30vw';
+
+    document.body.appendChild(header);
+    header.appendChild(pokemonLogo);
+}
+
+function criarDivFilter() {
+    const main = document.createElement('main');
+    const divFilter = document.createElement('div');
+
+    document.body.appendChild(main);
+    main.appendChild(divFilter);
+
+    criarInput(divFilter);
+}
+
+function criarInput(div) {
+    const inputFilter = document.createElement('input');
+
+    inputFilter.id = 'inputFilter';
+    inputFilter.placeholder = 'Procurar pokémon...'
+
+    div.appendChild(inputFilter);
+
+    criarBotaoFilter(div);
+}
+
+function criarBotaoFilter(div) {
+    const botaoFilter = document.createElement('button');
+
+    botaoFilter.id = 'botaoFilter';
+    botaoFilter.innerText = 'Buscar';
+    botaoFilter.style.marginLeft = '10px'
+
+    div.appendChild(botaoFilter);
+
+    criarTabela();
+}
+
+function criarTabela() {
+    let tabelaAtual = document.querySelector('table');
+    const main = document.querySelector('main');
+
+    if (tabelaAtual) {
+        tabelaAtual.remove();
+    }
+
+    const tabela = document.createElement('table');
+
+    main.appendChild(tabela);
+
+    pokemonList();
+}
+
+function pokemonList() {
+    fetch('https://prof-poke-api.herokuapp.com/api/pokedex/').then(function (resultado) {
+        resultado.json().then(function (data) {
+            console.log('data: ', data);
+            data.forEach(function (e) {
+                inserirPokemon(e);
+            })
+        });
+    }).catch(function (erro) {
+        console.log('Erro: ', erro);
+    });
+
+}
+
+function inserirPokemon(pokemon) {
+    const tabela = document.querySelector('table');
+    const linha = document.createElement('tr');
+    const colunaImagem = document.createElement('td');
+    const colunaNome = document.createElement('td');
+
+    colunaImagem.appendChild(criarImagemPokemon(pokemon));
+    colunaNome.innerText = pokemon.name;
+
+    tabela.appendChild(linha);
+    linha.appendChild(colunaImagem);
+    linha.appendChild(colunaNome);
+}
+
+function criarImagemPokemon(pokemon) {
+    const imgPokemon = document.createElement('img');
+    imgPokemon.src = pokemon.url_icon;
+
+    return imgPokemon;
+}
+
+criarLogo();
+criarDivFilter();
